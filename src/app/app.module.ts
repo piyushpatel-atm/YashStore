113edafdaff8d899d84bc9a281a94484d921e32e
Akshatjain27 <akshatkumarjain27@gmail.com>
2023-01-06 02:00:43 +0530

    Added the full functional login and signup code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { AuthService } from './shared/services/auth.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from './materiL-module';
+import { MaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,19 +10,46 @@ import { AdminLoginComponent } from './LoginFolder/admin-login/admin-login.compo
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { enviroment } from 'src/enviroments/enviroments';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { PageNotFoundErrorComponent } from './shared/layouts/page-not-found-error/page-not-found-error.component';
+import { SignInComponent } from './LoginFolder/sign-in/sign-in.component';
+import { SignUpComponent } from './LoginFolder/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './LoginFolder/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './LoginFolder/verify-email/verify-email.component';
+import { DashboardComponent } from './LoginFolder/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarcompComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    PageNotFoundErrorComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MaterialModule,FormsModule,ReactiveFormsModule,HttpClientModule
+    BrowserAnimationsModule,MaterialModule,FormsModule,ReactiveFormsModule,HttpClientModule,
+    AngularFireModule.initializeApp(enviroment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyAvJzgvA58FH1m6oZTTz5e3iLKyE8CQ3ZI',
+      authDomain: 'auth-tutorial-e90d9.firebaseapp.com',
+      projectId: 'auth-tutorial-e90d9',
+      storageBucket: 'auth-tutorial-e90d9.appspot.com',
+      messagingSenderId: '908551803349',
+      appId: '1:908551803349:web:48ec593a0bda790391ed5c',
+    },
+  };
+  
