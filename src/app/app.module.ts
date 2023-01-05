e7f27f850dfd2730b37b022756d49dc953619ee2
piyushpatel-atm <piyush29041999@gmail.com>
2023-01-05 18:36:54 +0530

    change in navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarcompComponent } from './navbarcomp/navbarcomp.component';
+import { AdminLoginComponent } from './LoginFolder/admin-login/admin-login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarcompComponent
+    NavbarcompComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MaterialModule
+    BrowserAnimationsModule,MaterialModule,FormsModule,ReactiveFormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
